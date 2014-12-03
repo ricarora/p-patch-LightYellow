51e741e6b645f84d1efc74f82e5654da342e2ace
Rebecca Mark <rebecca.lmark@gmail.com>
2014-12-03 11:11:36 -0800

    created session create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def current_member
+    User.find_by(username: params[:user][:username])
+  end
+
   def create
+    if params[:provider] == "internal"
+      if current_member && current_member.authenticate(params[:user][:password])
+      session[:current_member] = current_member.id
+      else
+        render :new
+      end
+    else
+
+    end
   end
 
   def update
