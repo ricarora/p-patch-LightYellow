643406ec6cb9ce1786159015eff866dba085ac0c
Richa Arora <ricarora@gmail.com>
2014-12-02 16:05:06 -0800

    controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'comments/update'
+
+  get 'comments/edit'
+
+  get 'comments/destroy'
+
+  get 'comments/index'
+
+  get 'comments/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/edit'
+
+  get 'users/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'blogposts/new'
+
+  get 'blogposts/create'
+
+  get 'blogposts/update'
+
+  get 'blogposts/edit'
+
+  get 'blogposts/destroy'
+
+  get 'blogposts/index'
+
+  get 'blogposts/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
