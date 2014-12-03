85c9360652d121b2ba4f62e76bbc53430a3c5d72
Richa Arora <ricarora@gmail.com>
2014-12-03 10:42:52 -0800

    add create method in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'The User is successfully saved!'
+      session[:current_user_id] = @user.id
+      redirect_to user_profile_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -19,5 +27,11 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :username, :password, :password_confirmation)
   end
 end
