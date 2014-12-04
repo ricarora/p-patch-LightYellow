5c7107bd2b48824a59c8dbaf18b9f8cec52ac3f4
Rebecca Mark <rebecca.lmark@gmail.com>
2014-12-03 16:16:28 -0800

    created two private methods in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :logged_in
+
+  private
+
+  def current_member
+    @current_member ||= User.find(session[:current_member])
+  end
+
+  def logged_in
+    if current_member == nil
+      redirect_to root_path
+    end
+  end
+
 end
