c6747b84f15813b61292c4537cbc0c0b85ef9ca2
Rebecca Mark <rebecca.lmark@gmail.com>
2014-12-04 16:33:29 -0800

    event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,14 @@ class Event < ActiveRecord::Base
   validates :title, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+  validates :start_is_before_end, on: :create
+  validates :cannot_be_in_past, on: :create
+
+  def start_is_before_end
+    start_time > end_time
+  end
+
+  def cannot_be_in_past
+    start_time < Date.today
+  end
 end
