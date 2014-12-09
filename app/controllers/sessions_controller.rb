0d3e6f2539d4befaf244c475b4f2e403fe2efb19
Richa Arora <ricarora@gmail.com>
2014-12-08 20:45:48 -0800

    blog post create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
+  skip_before_filter :logged_in, only: :create
+
   def new
+
   end
 
   def current_member
